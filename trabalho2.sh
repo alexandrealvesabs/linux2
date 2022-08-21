1f9174c2ec40a98578365fd47eac050ba6d42353
alexandrealvesabs <alexandre.alves.abs@gmail.com>
2022-08-21 13:04:25 -0300

    atualizar

---
+++ trabalho2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Atualizando o servidor..."
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+
+echo "Baixando e copiando os arquivos da aplicação..."
+
+cd /tmp
+wget https://github.com/alexandrealvesabs/aracnideos/master.zip
+unzip master.zip
+cd aracnideos-master
+cp -R * /var/www/html/
+
